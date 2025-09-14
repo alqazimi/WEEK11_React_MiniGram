6e1c45ec99d182d83f31a30e9e81a8de431118a1
alqazimi <alqazimi@example.com>
2025-09-14 22:56:16 +0100

    nur

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,38 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Heart } from "lucide-react";
 
-// Use this heart icon to create the like button
-import { Heart } from 'lucide-react';
-
-function PostCard({ id, username, imageUrl, caption, likes, onLike }) {
+function PostCard({  post,  onLike }) {
+  console.log(post)
   return (
     <div className="post">
-           {/* Build your post card here */}
-  
-      <div className="post-header">
-        <div className="avatar">{username.charAt(0).toUpperCase()}</div>
-        <div className="username">{username}</div>
+     {/* Build your post card here */}
+     <h2 className="post-username">{post.username}</h2>
+     <img className="post-image" src={post.imageUrl}/>
+     <p className="post-caption">{post.caption}</p>
+     <p className="post-likes">{post.likes}</p>
+     <button className="like-button" onClick={()=> onLike(post.id)}>
+      <Heart size={20}/> 
+     </button>
       </div>
-
-      <img src={imageUrl} alt={caption} className="post-image" />
-
-      <div className="post-actions">
-        <button
-          className={`like-button ${likes > 0 ? 'liked' : ''}`}
-          onClick={() => onLike(id)}
-        >
-          <Heart />
-        </button>
-        <div className="likes-count">
-          {likes} {likes === 1 ? 'like' : 'likes'}
-        </div>
-      </div>
-
-
-      <div className="post-caption">
-        <span className="username">{username}</span> {caption}
-      </div>
-    </div>
   );
 }
 
-export default PostCard;
+export default PostCard
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,27 +1,17 @@
 import React from 'react';
 
 // Import the PostCard here
-import PostCard from "./PostCard";
-
+import PostCard from "./PostCard"
 
 export function PostList({ posts, onLike }) {
   return (
     <div className="post-list">
      {/* Loop through the posts here and pass to the PostCard */}
-
-    {posts.map((post) => (
-      <PostCard
-       key={post.id}
-       id={post.id}
-       username={post.username}
-       imageUrl={post.imageUrl}
-       caption={post.caption}
-       likes={post.likes}
-       onLike={onLike}
-/>
+     {posts.map((post) => (
+        <PostCard key={post.id} post={post} onLike={onLike} />
       ))}
+
     </div>
   );
 }
-
-export default PostList;
+export default PostList
